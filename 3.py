e8cba41d06c9a0d134337988378a0b60d95c5e5b
colinlcrawford <ccolin84@gmail.com>
2020-06-08 20:01:09 -0400

    updates to prob 3 solution

--- 3.py
+++ 3.py
@@ -10,22 +10,29 @@ def largest_prime_factor(n: int) -> Optional[int]:
     """Get the largest prime factor of n."""
     # running number as we divide factors out of it
     num = n
+    max_prime = 0
 
-    # handle the case where n is prime
-    max_prime = n
-
-    # go through 2
+    # divide out 2
     while num % 2 == 0:
-        max_prime = 2
         num = int(num / 2)
+        max_prime = 2
 
-    # go through odd numbers
-    for i in range(3, int(math.sqrt(num) + 1), 2):
+    # the highest possible prime factor
+    max_factor = int(math.sqrt(num) + 1)
+
+    # counter for going through candidate prime numbers
+    i = 3
+    while max_prime < max_factor and num > 1:
         while num % i == 0:
             max_prime = i
             num = int(num / i)
+            max_factor = int(math.sqrt(num) + 1)
+        i += 2
 
-    return max_prime
+    if num == 1:
+        return max_prime
+
+    return num
 
 
 if __name__ == '__main__':
